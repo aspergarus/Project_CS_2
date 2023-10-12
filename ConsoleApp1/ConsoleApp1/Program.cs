8d71f7884b6ce90b7ad58ea7e1ce5d10c382239e
smoljankin <smoljankin@gmail.com>
2023-10-12 14:13:03 +0300

    Create function InitArray

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,6 +8,15 @@ namespace ConsoleApp1
 {
     class Program
     {
+        static void InitArray(int[] arr)
+        {
+            Random r = new Random();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = r.Next(100);
+            }
+        }
         static void Main(string[] args)
         {
         }
